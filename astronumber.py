f81b9f2412be0954d5f83692336c7fc614dcf913
Rahul Singh <144668592+rahulsinghpr@users.noreply.github.com>
2023-10-25 14:01:02 +0530

    Update astronumber.py

--- astronumber.py
+++ astronumber.py
@@ -1,21 +1,11 @@
-n = 131
-r=0
-while(n>0):
-    r = n%10
-    sum =(sum*10)+r
-    n = n/10
-
-r = 1
-sum = (13*10)+1
-sum =131
-n = 0
-
-r = 3
-sum = (1*10)+3
-sum =13
-n = 1
-
-r = 1
-sum = (0*10)+1
-sum =1
-n = 13
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+ digit = temp % 10
+ sum += digit ** 3
+ temp //= 10
+if num == sum:
+ print(num,"is an Armstrong number")
+else:
+ print(num,"is not an Armstrong number")
